8f19df55b3dc2655d339aa1cca29dfaf5ebe00c8
Yair Litman <yairlitman@gmail.com>
2020-05-06 18:54:59 +0200

    Formatting

--- ipi_tests/reg_test/test_runs.py
+++ ipi_tests/reg_test/test_runs.py
@@ -2,7 +2,9 @@ import subprocess as sp
 from pathlib import Path
 import pytest
 import time
-import os,shutil,tempfile
+import os
+import shutil
+import tempfile
 
 parent = Path(__file__).parent
 
@@ -12,12 +14,14 @@ cmd1_cmd2_folder_output = [
 ]
 
 
-def _run(tmp_path,cmd1, cmd2, cwd):
+def _run(tmp_path, cmd1, cmd2, cwd):
 
     try:
-        shutil.copytree(parent/cwd,tmp_path/cwd)
-        
-        ipi = sp.Popen(cmd1, cwd=(tmp_path/cwd), shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+        shutil.copytree(parent / cwd, tmp_path / cwd)
+
+        ipi = sp.Popen(
+            cmd1, cwd=(tmp_path / cwd), shell=True, stdout=sp.PIPE, stderr=sp.PIPE
+        )
         time.sleep(3)
         driver = sp.Popen(cmd2, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
         driver.wait()
@@ -37,16 +41,16 @@ def _run(tmp_path,cmd1, cmd2, cwd):
 def _check_error(ipi, driver):
     assert "" == ipi.communicate()[1].decode("ascii")
 
+
 @pytest.mark.parametrize("cmd1,cmd2,folder,file", cmd1_cmd2_folder_output)
-def test_cmd_and_files(tmp_path,cmd1, cmd2, folder, file):
+def test_cmd_and_files(tmp_path, cmd1, cmd2, folder, file):
     _run(tmp_path, cmd1, cmd2, folder)
 
 
 if __name__ == "__main__":
-     
-     for (cmd1, cmd2, folder, file) in cmd1_cmd2_folder_output:
-        tmpdir=tempfile.mkdtemp() 
-        print('Running {} in {}'.format(folder,Path(tmpdir)/folder))
-        test_cmd_and_files(Path(tmpdir),cmd1, cmd2, folder, file)
-        shutil.rmtree(tmpdir)   
 
+    for (cmd1, cmd2, folder, file) in cmd1_cmd2_folder_output:
+        tmpdir = tempfile.mkdtemp()
+        print("Running {} in {}".format(folder, Path(tmpdir) / folder))
+        test_cmd_and_files(Path(tmpdir), cmd1, cmd2, folder, file)
+        shutil.rmtree(tmpdir)
