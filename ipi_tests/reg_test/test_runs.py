408f14f181eb0c773a31a2871e9b28b89a3b1d88
Yair Litman <yairlitman@gmail.com>
2020-05-06 22:33:39 +0200

    asd

--- ipi_tests/reg_test/test_runs.py
+++ ipi_tests/reg_test/test_runs.py
@@ -9,29 +9,25 @@ import tempfile
 parent = Path(__file__).parent
 
 cmd1_cmd2_folder_output = [
-    [
-        "i-pi input.xml ",
-        "i-pi-driver -h localhost -p 33334 -m ch4hcbe",
-        "geop/bfgs",
-        "min.out",
-    ],
+    ["i-pi input.xml ", "i-pi-driver -h localhost -p 33334 -m ch4hcbe", "geop/bfgs", "min.out", ],
+    ["i-pi input.xml ", "i-pi-driver -u -m ch4hcbe", "geop/sd", "min.out", ],
 ]
 
 
 def _run(cmd1, cmd2, cwd):
 
     try:
-        # tmp_dir = Path(tempfile.mkdtemp())
-        # shutil.copytree(parent / cwd, tmp_dir / cwd)
+        tmp_dir = Path(tempfile.mkdtemp())
+        shutil.copytree(parent / cwd, tmp_dir / cwd)
 
-        # ipi = sp.Popen(cmd1, cwd=(tmp_dir / cwd), shell=True, stdout=sp.PIPE, stderr=sp.PIPE )
-        ipi = sp.Popen(cmd1, cwd=(parent / cwd), shell=True, stderr=sp.PIPE)
+        ipi = sp.Popen(cmd1, cwd=(tmp_dir / cwd), shell=True, stdout=sp.PIPE, stderr=sp.PIPE )
+        # ipi = sp.Popen(cmd1, cwd=(parent / cwd), shell=True, stderr=sp.PIPE)
         time.sleep(3)
         driver = sp.Popen(cmd2, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
         # driver.wait()
         # ipi.wait()
         _check_error(ipi, driver)
-        # shutil.rmtree(tmp_dir)
+        shutil.rmtree(tmp_dir)
 
     except sp.TimeoutExpired:
         raise RuntimeError(
