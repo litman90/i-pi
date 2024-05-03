24d50f730b6d7db7ee1d6c2ef4a453b93eef5d97
Yair Litman <yairlitman@gmail.com>
2024-05-03 16:32:50 +0100

    lint

--- drivers/py/pes/lammps.py
+++ drivers/py/pes/lammps.py
@@ -11,18 +11,20 @@ ERROR_MSG = """
 lammps  driver requires specification of the inputfile
 Example: python driver.py -m lammps -u -o <inputfile>
 """
-from lammps import lammps, LAMMPS_INT, LMP_STYLE_GLOBAL, LMP_VAR_EQUAL, LMP_VAR_ATOM
+from lammps import lammps, LMP_VAR_EQUAL
+
+# from laamps import  LAMMPS_INT, LMP_STYLE_GLOBAL, LMP_VAR_ATOM
 from ctypes import c_double
 
-class lammps_driver(Dummy_driver):
 
+class lammps_driver(Dummy_driver):
     def __init__(self, args=None, verbose=False):
         super(lammps_driver, self).__init__(args, error_msg=ERROR_MSG)
 
         self.initialize_lammps()
 
     def initialize_lammps(self):
-        """ Initialize the lammps driver """ 
+        """Initialize the lammps driver"""
         self.lmp = lammps()
         self.lmp.file(self._inputfile)
         self.natoms = self.lmp.get_natoms()
@@ -35,38 +37,38 @@ class lammps_driver(Dummy_driver):
         else:
             sys.exit(self.error_msg)
 
-    def send_info(self,cell, pos):
-        """ Send information to Lammps """
-        n3 = self.natoms*3
-        x = (n3*c_double)()
+    def send_info(self, cell, pos):
+        """Send information to Lammps"""
+        n3 = self.natoms * 3
+        x = (n3 * c_double)()
         for i in range(n3):
             x[i] = pos.flatten()[i]
         self.lmp.scatter_atoms("x", 1, 3, x)
 
     def get_info(self):
-        """ Get information from Lammps """
-        pot = self.lmp.extract_variable("eng",None, LMP_VAR_EQUAL)
-        f = self.lmp.gather_atoms("f",1,3)
+        """Get information from Lammps"""
+        pot = self.lmp.extract_variable("eng", None, LMP_VAR_EQUAL)
+        f = self.lmp.gather_atoms("f", 1, 3)
         pot = self.lmp.extract_compute("thermo_pe", 0, 0)
-        vir = np.eye(3)*0.0 #self.lmp.extract_compute("thermo_press", 0, 0)
-        force = np.zeros((self.natoms*3))
-        for i in range(3*self.natoms):
+        vir = np.eye(3) * 0.0  # self.lmp.extract_compute("thermo_press", 0, 0)
+        force = np.zeros((self.natoms * 3))
+        for i in range(3 * self.natoms):
             force[i] = f[i]
-        
+
         return pot, force, vir
 
     def __call__(self, cell, pos):
-        """Lammps potential """ 
+        """Lammps potential"""
 
-        print('\n')
-        print('\n')
-        print('here0',self.natoms,pos.shape)
-        print('\n')
-        print('\n')
-        self.send_info(cell,pos)
+        print("\n")
+        print("\n")
+        print("here0", self.natoms, pos.shape)
+        print("\n")
+        print("\n")
+        self.send_info(cell, pos)
         self.lmp.command("run 0")
-        pot,force, vir = self.get_info()
-        print('here',self.natoms,pos.shape,force.shape)
-        #vir = cell * 0.0  # makes a zero virial with same shape as cell
+        pot, force, vir = self.get_info()
+        print("here", self.natoms, pos.shape, force.shape)
+        # vir = cell * 0.0  # makes a zero virial with same shape as cell
         extras = "nada"
         return pot, force, vir, extras
